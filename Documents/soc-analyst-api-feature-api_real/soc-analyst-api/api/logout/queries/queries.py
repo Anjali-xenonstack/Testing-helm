d526558c309482953e22b4b9b57a0372db322e03
Anjali-xenonstack <anjali.priya@xenonstack.com>
2024-12-12 18:47:02 +0530

    Delete Documents/soc-analyst-api-feature-api_real/soc-analyst-api directory

--- Documents/soc-analyst-api-feature-api_real/soc-analyst-api/api/logout/queries/queries.py
+++
@@ -1,9 +0,0 @@
-Logout_query = """
-UPDATE public.sessionlog
-SET isactive = 'false'
-WHERE token =%s;
-
-"""
-Check_token_query = """
-SELECT token FROM public.sessionlog WHERE token = %s and isactive = 'false';
-"""
